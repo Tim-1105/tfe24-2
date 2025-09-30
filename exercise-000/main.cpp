da321c5a374c2e0bd6a314db23b2d66c5d5f7495
Tim Deuringer <deuringer.tim-it24@it.dhbw-ravensburg.de>
2025-09-30 15:15:01 +0200

    Solution Exercise-000

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -3,6 +3,21 @@
 
 #include "config.h"
 
+//Funktionen zu Teil 3
+
+void changeValueByParameter(int value)
+    {
+        value = 99;
+        fmt::print("Inside changeValueByParameter, value is: {}\n", value);
+    }
+
+void changeValueByPointer(int* ptr)
+    {
+        *ptr = 77;
+        fmt::print("Inside changeValueByPointer, value is: {}\n", *ptr);
+    }
+
+
 auto main(int argc, char **argv) -> int
 {
 
@@ -14,13 +29,39 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello World, {}!\n", tfe24::PROJECT_NAME);
 
     /* INSERT YOUR CODE HERE */
+
+    //Teil 1: Zeiger-Grundlagen
+
     int x = 10;
     int* p = &x;
-    int y = 20;
-    p=&y;
-    int* p2 = &x;
-    int foo = *p + *p2;
-    fmt::print("The value of loo is: {}\n", foo); 
+    fmt::print("The value of x is: {}\n", x);
+    fmt::print("The address of x is: {}\n", fmt::ptr(&x));
+    
+    fmt::print("The value of p is: {}\n", *p);
+    fmt::print("The address of p is: {}\n", fmt::ptr(p));
 
+    *p = 42;
+    fmt::print("The new value of x is: {}\n", x);
+    fmt::print("The new value of p is: {}\n", *p);
+
+    //Teil 2:Dynamische Speicherverwaltung
+
+    double* pi = new double(3.1415);
+    fmt::print("The value of pi is: {}\n", *pi);
+    fmt::print("The address of pi is: {}\n", fmt::ptr(pi));
+    delete pi;
+
+    //Teil 3: Funktionen mit Zeigern
+
+    int y = 5;
+    fmt::print("Before changeValueByParameter, y is: {}\n", y);
+    changeValueByParameter(y);
+    fmt::print("After changeValueByParameter, y is: {}\n", y);
+
+    fmt::print("Before changeValueByPointer, y is: {}\n", y);
+    changeValueByPointer(&y);
+    fmt::print("After changeValueByPointer, y is: {}\n", y);
+
+    
     return 0; /* exit gracefully*/
 }
