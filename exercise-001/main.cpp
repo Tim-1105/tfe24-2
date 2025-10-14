4937b9da46d43014b7f284b198aa098bc831d296
Tim Deuringer <deuringer.tim-it24@it.dhbw-ravensburg.de>
2025-10-14 13:37:45 +0200

    Solution Exercise-001

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -3,6 +3,18 @@
 
 #include "config.h"
 
+void changeValueByParameter(int value) //Funktionsdeklaration
+    {
+        value = 99;
+        fmt::print("Inside changeValueByParameter, value is: {}\n", value);
+    }
+    
+void changeValueByReference(int& ref) //Funktionsdeklaration
+    {
+        ref = 77;
+        fmt::print("Inside changeValueByReference, value is: {}\n", ref);
+    }
+
 auto main(int argc, char **argv) -> int
 {
 
@@ -16,10 +28,22 @@ auto main(int argc, char **argv) -> int
     /* INSERT YOUR CODE HERE */
 
     //Teil 1: Grundlagen Referenzen:
-
     int x = 10;
     int& r = x; //Referenz r auf x
-    
+    fmt::print("Werte von x und r: x = {}, r = {}\n", x, r);
+    r = 42; //Ändern von x über referenz r
+    fmt::print("Werte von x und r nach Änderung über r: x = {}, r = {}\n", x, r);
+
+
+    //Teil 2: Funktionen mit Referenzen:
+    int y = 5;
+    fmt::print("Before changeValueByParameter, y is: {}\n", y);
+    changeValueByParameter(y);
+    fmt::print("After changeValueByParameter, y is: {}\n", y);
+
+    fmt::print("Before changeValueByReference, y is: {}\n", y);
+    changeValueByReference(y);
+    fmt::print("After changeValueByReference, y is: {}\n", y);
 
     return 0; /* exit gracefully*/
 }
