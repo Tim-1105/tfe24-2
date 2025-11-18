88b3beafa379b20098c211b8b65ee6cac94218cf
Tim Deuringer <deuringer.tim-it24@it.dhbw-ravensburg.de>
2025-11-18 14:39:02 +0100

    Solution Exercise-010 with Tests

--- exercise-010/tests/001-TestCase.cpp
+++ exercise-010/tests/001-TestCase.cpp
@@ -2,3 +2,43 @@
 // And write tests in the same file:
 #include <catch2/catch_all.hpp>
 #include "myvector.hpp"
+
+TEST_CASE("basic operations on tfe24::myvector<int>", "[myvector][basic]") {
+  tfe24::myvector<int> v;
+  REQUIRE(v.size() == 0);
+
+  SECTION("push_back grows size") {
+    v.push_back(42);
+    v.push_back(7);
+    REQUIRE(v.size() == 2);
+    REQUIRE(v[0] == 42);
+    REQUIRE(v.at(1) == 7);
+  }
+
+  SECTION("resize larger fills with default values") {
+    v.resize(5);
+    REQUIRE(v.size() == 5);
+    for (size_t i = 0; i < v.size(); ++i) {
+      REQUIRE(v[i] == int{}); // erwartet 0 für int
+    }
+  }
+
+  SECTION("at throws on out of range") {
+    v.push_back(1);
+    REQUIRE_THROWS_AS(v.at(5), std::out_of_range);
+  }
+}
+
+TEST_CASE("copy semantics (Rule of Three)", "[myvector][copy]") {
+  tfe24::myvector<int> a;
+  for (int i = 0; i < 3; ++i) a.push_back(i + 10);
+  tfe24::myvector<int> b = a;          // Copy-Konstruktor
+  tfe24::myvector<int> c; c = a;       // Copy-Zuweisung
+
+  REQUIRE(b.size() == a.size());
+  REQUIRE(c.size() == a.size());
+
+  a[0] = 99; // Änderungen dürfen Kopien nicht beeinflussen
+  REQUIRE(b[0] != a[0]);
+  REQUIRE(c[0] != a[0]);
+}
